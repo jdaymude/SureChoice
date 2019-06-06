e5b4434203f9795847bac1cb059ce4b1294112c2
Josh Daymude <joshdaymude@gmail.com>
2019-06-06 00:41:28 -0700

    + Added first version of the comparison script, work-in-progress README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-SureChoice
+# SureChoice
+
+Understanding health insurance plans can be difficult, and comparing the pros and cons of different plans can be especially complex. This Python script plots your out-of-pocket cost for different plans as a function of total annual incurred medical costs so you can make better-informed decisions about your health insurance plan.
+
+![example plot](https://github.com/jdaymude/SureChoice/blob/master/_assets/example_plot.png "example plot")
+
+Some plans may be quite cost-effective if your total medical costs are low, but will be extremely costly if an expensive medical event happens unexpectedly. Some scale more linearly. Others cost a lot out-of-pocket up front, but pay off with higher total medical costs. Comparing plans in this way enables you to choose the best plan for your expected medical costs.
+
+
+## Background and Key Terms
+
+When using this code, it helps to understand a few key health insurance terms:
+
+- _premium_: a fixed monthly payment for being on a plan.
+- _deductible_: the total out-of-pocket amount to be paid before insurance covers anything.
+- _copay_: the percentage of a total cost to be paid out-of-pocket after the deductible is met.
+- _maximum out-of-pocket_: the maximum total out-of-pocket cost before insurance covers 100% of all subsequent payments.
+
+
+## Installation
+
+[TODO: installation and dependencies]
+
+
+## Preparing Plans for Comparison
+
+[TODO: data format]
+
+
+## Running SureChoice
+
+[TODO: run commands]
+
+
+## Feedback & Contibutions
+
+[TODO: typical stuff about opening issues/pull requests]
---
+++ surechoice.py
@@ -0,0 +1,48 @@
+# Project:     SureChoice
+# Filename:    surechoice.py
+# Authors:     Joshua J. Daymude (jdaymude@asu.edu)
+# Description: [TODO]
+
+import argparse
+import json
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == '__main__':
+    """
+    SureChoice: Make informed decisions about health insurance.
+    """
+    # Parse arguments.
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--data', required=True, help='JSON file of plans data')
+    parser.add_argument('--max_annual_cost', default=30000, help='Maximum ' \
+                        + 'total annual medical cost to consider')
+    args = parser.parse_args()
+
+    # Load plans data.
+    with open(args.data) as f_in:
+        data = json.load(f_in)
+
+    # Compute costs for each plan.
+    oop_costs = {}
+    amnts = [i for i in range(int(args.max_annual_cost) + 1)]
+    for plan in data['plans']:
+        plan_oop = []
+        max_oop = math.inf if plan['max_oop'] == 'None' else plan['max_oop']
+        for amnt in amnts:
+            prem_cost = plan['premium'] * 12
+            to_deduct = min(amnt, plan['deductible'])
+            remaining = (amnt - to_deduct) * plan['copay']
+            plan_oop.append(prem_cost + min(to_deduct + remaining, max_oop))
+        oop_costs[plan['short_name']] = plan_oop
+
+    # Plot the results.
+    fig, ax = plt.subplots()
+    for plan in data['plans']:
+        ax.plot(amnts, oop_costs[plan['short_name']], label=plan['short_name'])
+    ax.set(title='Health Insurance Comparison', xlabel='Annual Medical ' \
+           + 'Costs ($)', ylabel='Out-of-Pocket Costs ($)')
+    ax.legend(loc='upper left')
+    ax.grid()
+    fig.savefig('plot.png', dpi=300)
